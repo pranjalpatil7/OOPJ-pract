bd9238e0ae492f69a804114b885b84d8c67411ad
pranjalpatil7 <pranjal.236297208@vcet.edu.in>
2024-08-09 14:05:01 +0530

    Add files via upload

---
+++ 45 Pranjal/Conditional.java
@@ -0,0 +1,15 @@
+class Conditional
+{
+	public static void main (String args[])
+	{
+	int a=10;
+	if(a<15)
+	{
+		System.out.println("a is less number than ");
+	}
+	else
+	{
+		System.out.println("a is not less than 15");
+	}
+	}
+}	
